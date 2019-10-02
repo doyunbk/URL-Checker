3aa8f8814e7bad5df212c5f0b6fe92d6cb280938
Doyun <doyun@Doyuns-iMac.hitronhub.home>
2019-10-02 13:37:54 -0700

    Implement GET method for getting the values of URL from Redis

--- model/redis_pool.go
+++ model/redis_pool.go
@@ -1,12 +1,15 @@
 package redis
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/gomodule/redigo/redis"
 )
 
 var pool *redis.Pool
+
+var NoUrlFound = errors.New("no url found")
 
 // Initialize the pool of Redis database
 // for creating and configuring a connection
@@ -29,7 +32,39 @@ func Ping(c redis.Conn) error {
 		return err
 	}
 
-	fmt.Printf("PING %s : Redis Server Connected \n", s)
+	fmt.Printf("PONG %s : Redis Connected \n", s)
 
 	return nil
+}
+
+// Get Redis pool
+func GetPool() *redis.Pool {
+	if pool == nil {
+		pool = InitPool()
+	}
+	return pool
+}
+
+// GetURL is getting the values of URL from Redis database by searching keys
+func GetURL(url string) (*URL, error) {
+	conn := GetPool().Get()
+
+	defer conn.Close()
+
+	// Lookup all fields and values of urls in the database
+	urls, err := redis.Values(conn.Do("HGETALL", ""+url))
+	if err != nil {
+		return nil, err
+	} else if len(urls) == 0 {
+		return nil, NoUrlFound
+	}
+
+	// Scan the struct of values of urls
+	var lookupurl URL
+	err = redis.ScanStruct(urls, &lookupurl)
+	if err != nil {
+		return nil, err
+	}
+
+	return &lookupurl, nil
 }
